9a6487a8c6082983c7b4acb45939091ccf9d5c1f
Igor Alexandre Goto <gotinho@users.noreply.github.com>
2022-01-20 22:35:53 -0300

    call airline deposit from dapp

--- src/dapp/index.js
+++ src/dapp/index.js
@@ -4,7 +4,7 @@ import Contract from './contract';
 import './flightsurety.css';
 
 
-(async() => {
+(async () => {
 
     let result = null;
 
@@ -12,22 +12,43 @@ import './flightsurety.css';
 
         // Read transaction
         contract.isOperational((error, result) => {
-            console.log(error,result);
-            display('Operational Status', 'Check if contract is operational', [ { label: 'Operational Status', error: error, value: result} ]);
+            console.log(error, result);
+            display('Operational Status', 'Check if contract is operational', [{ label: 'Operational Status', error: error, value: result }]);
         });
-    
+
 
         // User-submitted transaction
         DOM.elid('submit-oracle').addEventListener('click', () => {
             let flight = DOM.elid('flight-number').value;
             // Write transaction
             contract.fetchFlightStatus(flight, (error, result) => {
-                display('Oracles', 'Trigger oracles', [ { label: 'Fetch Flight Status', error: error, value: result.flight + ' ' + result.timestamp} ]);
+                display('Oracles', 'Trigger oracles', [{ label: 'Fetch Flight Status', error: error, value: result.flight + ' ' + result.timestamp }]);
             });
-        })
-    
+        });
+
+
+        contract.airlines.forEach((address, i) => {
+            DOM.elid('airlines').append(DOM.makeElement('option', { value: address }, 'Airline ' + i));
+            DOM.elid('airlines2').append(DOM.makeElement('option', { value: address }, 'Airline ' + i));
+        });
+
+        // Deposit 
+        DOM.elid('btn-airline-deposit').onclick = async () => {
+            const value = DOM.elid('deposit-value').value;
+            const asAirline = DOM.elid('airlines').value;
+
+            await contract.deposit(asAirline, value);
+        };
+
+        // Register airline
+        DOM.elid('btn-register-airline').onclick = async () => {
+            const asAirline = DOM.elid('airlines').value;
+            const forAirline = DOM.elid('airlines2').value;
+            await contract.registerAirline(asAirline, forAirline);
+        }
+
     });
-    
+
 
 })();
 
@@ -38,16 +59,14 @@ function display(title, description, results) {
     section.appendChild(DOM.h2(title));
     section.appendChild(DOM.h5(description));
     results.map((result) => {
-        let row = section.appendChild(DOM.div({className:'row'}));
-        row.appendChild(DOM.div({className: 'col-sm-4 field'}, result.label));
-        row.appendChild(DOM.div({className: 'col-sm-8 field-value'}, result.error ? String(result.error) : String(result.value)));
+        let row = section.appendChild(DOM.div({ className: 'row' }));
+        row.appendChild(DOM.div({ className: 'col-sm-4 field' }, result.label));
+        row.appendChild(DOM.div({ className: 'col-sm-8 field-value' }, result.error ? String(result.error) : String(result.value)));
         section.appendChild(row);
     })
     displayDiv.append(section);
 
 }
-
-
 
 
 
